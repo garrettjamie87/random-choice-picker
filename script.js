943bb5921a7cac4e0fda71209d38e2d03a3b4316
Jamie Garrett <garrettjamie87@googlemail.com>
2021-02-25 18:54:41 +0100

    add split, filter, trim and map to take away any excess unwanted spaces

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ textarea.focus()
 textarea.addEventListener('keyup', (e)=>{
       createTags(e.target.value)
 })
+
+function createTags(input){
+      const tags = input.split(',').filter(tag=>tag.trim()
+      !=='').map(tag => tag.trim())
+      // console.log(tags)
+
+      tagsEl.innerHTML = ''
+}
