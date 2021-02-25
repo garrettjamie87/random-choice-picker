a5365f70f92b8ced175c0c1d3e1c5810a07e913a
Jamie Garrett <garrettjamie87@googlemail.com>
2021-02-25 19:09:59 +0100

    add pickRandomTag function

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
-const tagsEl = document.getElementById('tags')
-const textarea = document.getElementById('textarea')
+const tagsEl = document.getElementById("tags");
+const textarea = document.getElementById("textarea");
 
-textarea.focus()
-
-textarea.addEventListener('keyup', (e)=>{
-      createTags(e.target.value)
-})
-
-function createTags(input){
-      const tags = input.split(',').filter(tag=>tag.trim()
-      !=='').map(tag => tag.trim())
-      // console.log(tags)
-
-      tagsEl.innerHTML = ''
+textarea.focus();
 
 
-tags.forEach(tag => {
-      const tagEl = document.createElement('span')
-      tagEl.classList.add('tag')
-      tagEl.innerText = tag
-      tagsEl.appendChild(tagEl)
-      
-})
+textarea.addEventListener("keyup", (e) => {
+  createTags(e.target.value);
+
+  if (e.key === "Enter") {
+    setTimeout(() => {
+      e.target.value = "";
+    }, 10);
+
+    randomSelect();
+  }
+});
+
+
+function createTags(input) {
+  const tags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim());
+  // console.log(tags)
+
+  tagsEl.innerHTML = "";
+
+  tags.forEach((tag) => {
+    const tagEl = document.createElement("span");
+    tagEl.classList.add("tag");
+    tagEl.innerText = tag;
+    tagsEl.appendChild(tagEl);
+  });
+}
+
+function randomSelect() {
+      // console.log(123)
+      const times = 30
+
+      const interval = setInterval(()=>{
+            const randomTag = pickRandomTag()
+      }, 100)
+}
+
+function pickRandomTag() {
+      const tags = document.querySelectorAll('.tag')
+      return tags[Math.floor(Math.random() * tags.length)]
 }
